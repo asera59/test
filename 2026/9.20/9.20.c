b594a8e552ee564f20992831bc02ae16c7afbf95
asera59 <1292776499@qq.com>
2026-09-20 17:25:01 +0800

    启动段 D3：Keil 仿真跑通 + Keil 工程文件排除规则

---
+++ 2026/9.20/9.20.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+
+int main()
+{
+//	int a = 0;
+//	int b = 0;
+//	
+//	scanf("%d %d", &a, &b);
+//	
+//	printf("%d %d\n", a, b);
+//	const int AMOUNT = 100;
+//	int price = 0;
+//	
+//	printf("请输入金额（元）："); 
+//	scanf("%d", &price);
+//	
+//	int change = AMOUNT - price;
+//	
+//	printf("找您%d元。\n", change);
+//int a, b;
+//
+//printf("请输入两个整数：");
+//scanf("%d %d", &a, &b);
+//printf("%d + %d = %d\n", a, b, a+b);
+//	printf("请分别输入身高的英尺和英寸，"
+//		"如输入\"5 7\"表示5英尺7英寸：");
+//
+//	double foot;
+//	double inch;
+//
+//	printf("身高是%f米。\n", 
+//		((foot + inch / 12) * 0.3048));
+//int a = 7,b = 12;
+//
+//printf("把两个整数相除：%d\n", a / b);
+//printf("把一个数写成小数： %f\n", a / 12.0) ;
+//
+//printf("char 占%d个字节\n", sizeof(char));
+//printf("int 占%d个字节\n", sizeof(int));
+//printf("double 占 %d个字节\n", sizeof(double));
+int i = 0;
+while (1)
+{
+	i=i+1;
+	printf("i=%d\n", i);
+//	if (i >= 5)break;
+	if(i >= 5) continue;
+	printf("  （这一圈还没结束）\n"); 
+}
+	printf("循环结束了， i= %d\n",i);
+	return 0; 
+}
---
+++ 2026/9.20/test/main.c
@@ -0,0 +1,10 @@
+#include <reg52.h>
+
+void main(void)
+{
+	unsigned char i = 0;
+	while (1)
+	{
+	i = i + 1;
+	}
+	}
